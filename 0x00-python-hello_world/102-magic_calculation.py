2f5c1a7b3bae29984f7a3e19ecd32c5d9ff57b77
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-02 07:34:59 -0800

    Bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-        result = 98
-            result = result ** a
-                result = result + b
-                    return result
+        return (98 + a ** b)
