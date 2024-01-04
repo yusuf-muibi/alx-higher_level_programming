303cb44f3f1e85c4f5008598ff0faaf14e8e1328
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-04 06:50:44 -0800

    Calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,14 +4,12 @@ b = 5
 
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    result_add = add(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = div(a, b)
+def main():
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
-    print("{} + {} = {}".format(a, b, result_add))
-    print("{} - {} = {}".format(a, b, result_sub))
-    print("{} * {} = {}".format(a, b, result_mul))
-    print("{} / {} = {}".format(a, b, result_div))
-    
+if __name__ == "__main__":
+    main()
+        
