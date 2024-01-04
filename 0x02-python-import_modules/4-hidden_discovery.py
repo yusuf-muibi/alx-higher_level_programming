73ce70c25110307039be3329a5ba98a9b17bce27
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-04 10:28:25 -0800

    Hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    def print_hidden_names():
+
+        import hidden_4
+
+        names = sorted(name for name in dir(hidden_4) if not name.startswith("__"))
+        for name in names:
+            print(name)
+
+    print_hidden_names()
