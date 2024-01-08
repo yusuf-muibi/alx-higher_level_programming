114031971e7e1f282fa4227788691279153a2864
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-08 12:51:36 -0800

    Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,51 +4,61 @@
 #include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
+* reverse_list - Reverses a linked list in-place.
+* @head: Pointer to the head of the linked list.
+* Return: Pointer to the new head of the reversed list.
 */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *reverse_list(listint_t *head)
 {
-listint_t *new;
+listint_t *prev = NULL;
+listint_t *current = head;
+listint_t *next;
 
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-new->next = *head;
-*head = new;
-return (new);
+while (current != NULL)
+{
+next = current->next;
+current->next = prev;
+prev = current;
+current = next;
+}
+
+return (prev);
 }
 /**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
+* is_palindrome - Checks if a singly linked list is a palindrome.
+* @head: Pointer to the head of the linked list.
+* Return: 1 if it is a palindrome, 0 otherwise.
 */
 int is_palindrome(listint_t **head)
 {
-listint_t *head2 = *head;
-listint_t *aux = NULL, *aux2 = NULL;
-
-if (*head == NULL || head2->next == NULL)
+if (*head == NULL || (*head)->next == NULL)
 return (1);
-while (head2 != NULL)
+
+listint_t *slow = *head;
+listint_t *fast = *head;
+listint_t *prev = NULL;
+listint_t *next;
+
+while (fast != NULL && fast->next != NULL)
 {
-add_nodeint(&aux, head2->n);
-head2 = head2->next;
+fast = fast->next->next;
+
+next = slow->next;
+slow->next = prev;
+prev = slow;
+slow = next;
 }
-aux2 = aux;
-while (*head != NULL)
+
+if (fast != NULL)
+slow = slow->next;
+
+while (prev != NULL && slow != NULL)
 {
-if ((*head)->n != aux2->n)
-{
-free_listint(aux);
+if (prev->n != slow->n)
 return (0);
+prev = prev->next;
+slow = slow->next;
 }
-*head = (*head)->next;
-aux2 = aux2->next;
-}
-free_listint(aux);
+
 return (1);
 }
