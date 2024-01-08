3a923f6552a509d7d4f39773c25e8d52971cda81
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-08 12:43:38 -0800

    Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,54 @@
+#include "lists.h"
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "lists.h"
 
 /**
-* reverse_list - Reverses a linked list.
-* @head: Pointer to the head of the linked list.
-* Return: Pointer to the new head of the reversed list.
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
 */
-listint_t *reverse_list(listint_t *head)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *prev = NULL;
-listint_t *current = head;
-listint_t *next;
+listint_t *new;
 
-while (current != NULL)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
+return (new);
 }
-
-return (prev);
-}
-
 /**
-* is_palindrome - Checks if a singly linked list is a palindrome.
-* @head: Pointer to the head of the linked list.
-* Return: 1 if it is a palindrome, 0 otherwise.
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
 */
 int is_palindrome(listint_t **head)
 {
-if (*head == NULL || (*head)->next == NULL)
+listint_t *head2 = *head;
+listint_t *aux = NULL, *aux2 = NULL;
+
+if (*head == NULL || head2->next == NULL)
 return (1);
-
-listint_t *slow = *head;
-listint_t *fast = *head;
-
-while (fast != NULL && fast->next != NULL)
+while (head2 != NULL)
 {
-slow = slow->next;
-fast = fast->next->next;
+add_nodeint(&aux, head2->n);
+head2 = head2->next;
 }
-
-listint_t *second_half = reverse_list(slow);
-listint_t *first_half = *head;
-
-while (second_half != NULL)
+aux2 = aux;
+while (*head != NULL)
 {
-if (first_half->n != second_half->n)
+if ((*head)->n != aux2->n)
 {
-reverse_list(second_half);
+free_listint(aux);
 return (0);
 }
-first_half = first_half->next;
-second_half = second_half->next;
+*head = (*head)->next;
+aux2 = aux2->next;
 }
-
-reverse_list(second_half);
+free_listint(aux);
 return (1);
 }
