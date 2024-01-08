d48301cc983a2f3b9d5f1b30de48caf79a731f87
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-08 06:40:30 -0800

    C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    result_string = ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
+    result_string = ""
+    for char in my_string:
+        result_string += char if char.lower() not in {'c', 'C'} else ""
     return result_string
