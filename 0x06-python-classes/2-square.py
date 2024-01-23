4fee793d6cf01032bdb15770f0a4fc29d6ada8c7
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-23 05:53:26 -0800

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -21,10 +21,8 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        # Check if size is an integer
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        # Check if size is less than 0
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
