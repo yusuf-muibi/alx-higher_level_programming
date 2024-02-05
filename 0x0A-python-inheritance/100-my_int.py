5152a6cbe53b64ca1b74e6f6259b79ffed96cf2e
yusuf-muibi <muibiyusuf@hotmail.com>
2024-02-05 11:49:19 -0800

    MyInt

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return super().__eq__(value)
