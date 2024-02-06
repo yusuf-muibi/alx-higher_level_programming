4d7bb8b5d3c139a57463b741a3edacb36fa505d1
yusuf-muibi <muibiyusuf@hotmail.com>
2024-02-06 05:48:27 -0800

    Add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This function adds all arguments to a Python list and save them to a file"""
+"""Adds all arguments to a Python list and save them to a file"""
 import sys
 
 if __name__ == "__main__":
