51b21c296d3bd6765671701e0823b800f1e53083
yusuf-muibi <muibiyusuf@hotmail.com>
2024-02-06 05:31:50 -0800

    Add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """This function adds all arguments to a Python list and save them to a file"""
 import sys
 
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 try:
     existing_list = load_from_json_file("add_item.json")
