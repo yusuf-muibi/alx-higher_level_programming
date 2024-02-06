9bbed2e6269552197a9b680813be3db669177776
yusuf-muibi <muibiyusuf@hotmail.com>
2024-02-06 07:10:50 -0800

    Student Class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""Defines a student by public instance attributes."""
+"""Defines a class Student."""
 
 
 class Student:
-    """Defines a student with first name, last name, and age."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first name, last name, and age."""
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of a Student instance."""
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return{attr: getattr(self, attr) for attr in attrs if hasattr(self,attr)}
