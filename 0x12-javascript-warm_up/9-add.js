4f0a423672dc06062e00ab58a111fbac02116f27
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-08 13:59:04 -0700

    Addition

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+
 function add(a, b) {
-  const t = a + b;
-  console.log(t);
+  const sum = a + b;
+  console.log(sum);
 }
+
 const firstInteger = parseInt(process.argv[2]);
 const secondInteger = parseInt(process.argv[3]);
+
 add(firstInteger, secondInteger);
