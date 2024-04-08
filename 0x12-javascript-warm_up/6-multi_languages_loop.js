494935740f49d3fd1ca4ae3988f7c97345a7319b
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-08 13:01:22 -0700

    Using loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
-
-const lines = [
-  'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
-];
-
-let output = '';
-
-for (let i = 0; i < 3; ) {
-
-  output += lines[i];
-  output += '\n';
-  i++;
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
-
-console.log(output);
