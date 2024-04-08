cdd06352d2ce2c854876d734d4ac23e0772a354e
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-08 08:42:35 -0700

    Concatenation

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length !== 2) {
-  console.log('Usage: ./printArguments.js <argument1> <argument2>');
-} else {
-  const argument1 = args[0];
-  const argument2 = args[1];
+const argument1 = args[0];
+const argument2 = args[1];
 
-  // Print the formatted output
-  console.log(`${argument1} is ${argument2}`);
+console.log(`${argument1} is ${argument2}`);
 }
