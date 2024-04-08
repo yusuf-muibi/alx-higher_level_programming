7ab2856592c7f7605dd59cb6c284118f4dabc93b
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-08 14:17:35 -0700

    Second largest for a reason

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-
-const integers = process.argv.slice(2).map(Number);
-const numIntegers = integers.length;
-
-if (numIntegers === 0 || numIntegers === 1) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  const sortedIntegers = integers.sort((a, b) => b - a);
-  const secondBiggest = sortedIntegers[1];
-  
-  console.log(secondBiggest);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
