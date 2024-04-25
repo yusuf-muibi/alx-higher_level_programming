cb386ec03b8a27fceb84799301d7b6cad18b6e1d
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-25 15:19:37 -0700

    These really are advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and displays only the status code of the response.
-curl -so /dev/null -w "%{http_code}" "$1"
+# This script makes a request to 0.0.0.0:5000/catch_me_3 and captures the response.
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
