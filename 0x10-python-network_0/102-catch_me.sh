863a4ca501fdeaf9da4a92a00bc9401f0d7ecca8
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-25 15:07:06 -0700

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me and captures the response.
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "You got me!"
+curl -sL -X PUT -d "user_id=98" http://0.0.0.0:5000/catch_me_3
