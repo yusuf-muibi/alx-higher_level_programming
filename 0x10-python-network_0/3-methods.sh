9513c99fc999f8fe24c94840bc40dfebfddab9f5
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-25 12:51:02 -0700

    Method to accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument and displays all HTTP methods accepted by the server.
-curl -s -X OPTIONS -i "$1" | grep -i Allow | awk '{print $2}'
+curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
