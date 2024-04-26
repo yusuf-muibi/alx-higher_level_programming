37ea13e301f619bb3a399dbb76d79d6fd088fb23
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-26 15:10:11 -0700

    Repsosne header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,11 +4,9 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    import sys
+    import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.headers.get('X-Request-Id')
-    if x_request_id:
-        print(x_request_id)
-    else:
-        print("X-Request-Id header not found in the response.")
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])
