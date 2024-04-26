cbbb38552560e1ca4091e7db88eb8d2d0b745943
yusuf-muibi <muibiyusuf@hotmail.com>
2024-04-26 15:12:39 -0700

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and displays the value of the X-Request-Id variable."""
-import urllib.request
-import sys
+"""
+Sends a request to a URL and displays the value of the X-Request-Id variable.
+"""
 
 
 if __name__ == "__main__":
